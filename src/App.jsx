7a8c2563077b14ed1e798b8f65df6adbfb1c5aa3
uJoao22 <joaopaniciobotelho@hotmail.com>
2022-09-26 18:00:53 -0300

    React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import './assets/css/base/base.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import Home from './paginas/Home'
 import Sobre from './paginas/Sobre'
 
 function App() {
-  // Criando função para gerenciar se estou na rota sobre ou não e o que deve ser exibido
-  const Router = () => {
-    return window.location.pathname === '/sobre' ? <Sobre/> : <Home/>
-  }
-
   return (
-    <>
-      { Router() }
-    </>
+    // Envolvendo toda a aplicação em um componente de BrowserRouter para genrenciar as Rotas
+    // Envolvendo cada componente que tenho em um componente Route e definido o path que será usado para a visualização
+    <Router>
+
+      <Route path="/">
+        <Home/>
+      </Route>
+
+      <Route path="/sobre">
+        <Sobre/>
+      </Route>
+
+    </Router>
   )
 }
 
