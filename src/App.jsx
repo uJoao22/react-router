77724aa80c10a691be7d1677e7519601014b7cea
uJoao22 <joaopaniciobotelho@hotmail.com>
2022-09-28 17:42:19 -0300

    Criando NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Home from './paginas/Home'
 import Sobre from './paginas/Sobre'
 import Pagina404 from './paginas/Pagina404'
+import Cabecalho from './components/Cabecalho'
 
 function App() {
   return (
     // Envolvendo toda a aplicação em um componente de BrowserRouter para genrenciar as Rotas
     // Envolvendo cada componente que tenho em um componente Route e definido o path que será usado para a visualização
     <Router>
+
+      <Cabecalho/>
 
       {/* Usando para evitar o comportamento padrão do react-router que mesmo quando achar a rota que preciso, continuar procurando e correr risco de encontrar uma outra rota com o mesmo nome e exibir ambas */}
       <Switch>
