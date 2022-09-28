c16678f46d7c02343a78cb896d3d615107627b54
uJoao22 <joaopaniciobotelho@hotmail.com>
2022-09-28 18:15:46 -0300

    Exibindo os post

--- src/components/ListaPost.jsx
+++ src/components/ListaPost.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { busca } from "../api/api";
 
-const ListaPost = () => {
+const ListaPost = ({ url }) => {
+
+  const [posts, setPosts] = useState([])
+
+  // Realizando requisição em userEffect para não entrar em loop infito
+  // PESQUISAR MELHOR O FUNCIONAMENTO DE useEffect
+  useEffect(() => {
+    busca(url, setPosts)
+  }, [])
+
   return (
     <section className="posts container">
+      { posts.map((post) => (
+        <Link to="#" className={`cartao-post cartao-post--${post.categoria}`} key={post.id}>
 
+          <article>
+
+            <h3 className="cartao-post__titulo">
+              { post.title }
+            </h3>
+
+            <p className="cartao-post__meta">
+              { post.metadescription }
+            </p>
+
+          </article>
+
+        </Link>
+      ))}
     </section>
   )
 }
